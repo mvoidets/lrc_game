3acfb4dee0212439e3626281bc08182e4f9e6940
mvoidets <mvoidets@yahoo.com>
2025-02-15 11:52:27 -0700

    changed render web for chat in socketClient.ts

--- app/lib/socketClient.ts
+++ app/lib/socketClient.ts
@@ -2,7 +2,7 @@
 import { io } from "socket.io-client";
 
 
-const socket = io("https://chatsocket-9q6u.onrender.com");
+const socket = io("https://lrc-chat.onrender.com");
 //const socket = io("http://localhost:3000");  // Ensure this points to your server URL
 export { socket };
 
